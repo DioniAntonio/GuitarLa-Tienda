73202d4848eb910704de93d6548d65d29dd3f6ef
Dioni Martínez <dioniantonio10@gmail.com>
2024-06-30 16:47:26 -0400

    cambios a hooks personalizados

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,9 @@
-import { useMemo } from "react"
+import{useCart} from "../hooks/useCart"
 
-
-
-function Header({cart,removeFromCart,increaseQuantity, decreaseQuantity,clearCart}) {
+function Header({cart,removeFromCart,increaseQuantity, decreaseQuantity,clearCart, isEmpty,cartTotal}) {
     
-    const isEmpty = useMemo(() =>cart.length === 0, [cart])
-    const cartTotal = useMemo(() => cart.reduce((total,item) => total + (item.quantity * item.price), 0), [cart])
-    
+
+
     return  (
         <>
             <header className="py-5 header">
